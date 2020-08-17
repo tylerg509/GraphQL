87cd3586db20bbaa41454e8e0ea99066520086bb
Tyler Gasperlin <gasperlint@gmail.com>
2020-08-16 23:17:08 -0700

    Add root query

--- schema/schema.js
+++ schema/schema.js
@@ -2,8 +2,14 @@ const graphql = require('graphql')
 const {
     GraphQLObjectType,
     GraphQLInt,
-    GraphQLString
+    GraphQLString,
+    GraphQLSchema
 } = graphql;
+
+const users = [
+    { id: '23', firstName: 'Bill', age: 20},
+    { id: '47', firstName: 'Samantha', age: 21 }
+];
 
 // name = describe the type we are defining
 // fields =  tell graphql about all properties that the user has
@@ -14,4 +20,24 @@ const UserType = new GraphQLObjectType({
         firstName: {type: graphql.GraphQLString} ,
         age: {type: graphql.GraphQLInt}
     }
+})
+
+//  Allow GraphQl to jump to a specific node in the graph of all data
+// If you are looking for a user provide an id and I give you that User
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user:{
+            type: UserType,
+            args: { id: {type: GraphQLString }},
+            resolve(parentValue, args) {
+                return _.find(users, { id: args.id });
+            }
+        }
+
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-
+const { schema } = require('./schema/schema')
 
 const app = express();
 
 // graph ql is registered as a middleware (via app.use)
 app.use('/graphql', graphqlHTTP({
+    schema,
     graphiql: true
 }))
 
